fa251bc8eedd122d00c3fd1738b52735255b348b
Papamusha <58028041+Papamusha@users.noreply.github.com>
2023-02-06 23:58:31 +0000

    Data now reads correctly
    see above, hashtag now correctly reads string value.

--- src/useData.js
+++ src/useData.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
-import { csv, csvParse } from 'd3';
+import { csv } from 'd3';
 
 const csvUrl =
   'https://gist.githubusercontent.com/Papamusha/af9c1638e971c4321e507cd6f50b8ac4/raw/0199d50610efc4c03bafea7f98bc59f11fa96c61/smhashloc.csv';
 
 const row = d => {
   d.coordinates = d['coordinates'].split(',').map(d => +d).reverse();
-  d['hashtag'] = csvParse(JSON.stringify(d['hashtag']));
+  d['hashtag'] = d['hashtag'];
   d['date'] = new Date(d['date']);
   return d;
 };
